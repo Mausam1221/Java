3cda1c7b60944f23ce19a1ae6615e958bf24d89e
Mausam1221 <dahalmausam12@gmail.com>
2023-06-05 19:16:54 +0545

    all files

--- loop.java
+++
@@ -1,9 +0,0 @@
-//Write a Java program to print your name 100 times.
-public class loop{
-    public static void main(String[]args){
-        for(int i=1;i<=100;i++)
-        {
-            System.out.print("Hello World\t");
-        }
-    }
-}
--- without41.java
+++
@@ -1,9 +0,0 @@
-public class without41{
-    public static void main(String[]args){
-        for(int i=1;i<=100;i++)
-        {
-            if(i!=41)           
-            System.out.print("\t"+i);
-        }
-    }
-}
